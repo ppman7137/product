1379ad267cbf66deab62856f5a37604f7f50a1d0
Marc Huang <ppman7137@gmail.com>
2022-02-28 02:28:32 +0800

    add product.py

--- product.py
+++ product.py
@@ -2,6 +2,19 @@
 """
 Created on Wed Feb 16 01:49:32 2022
 
-@author: user
+@author: JYH
 """
+products = []
+while True:
+   name = input('Please key in product names:')
+   if name == 'q':
+       break
+   price = input('Please key in price:')
+   p = []
+   #p.append(name) # add name into p
+   #p.append(price) # add price into p
+   ##p = [name, price]
+   products.append([name, price]) # add p into product list
+print(products)
 
+print(products [0][0])
