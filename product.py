ce145375661fd670dca71e246472733374ce2858
Marc Huang <ppman7137@gmail.com>
2022-04-03 23:16:59 +0800

    add header

--- product.py
+++ product.py
@@ -10,15 +10,23 @@ while True:
    if name == 'q':
        break
    price = input('Please key in price:')
-   p = []
+   #p = []
    #p.append(name) # add name into p
    #p.append(price) # add price into p
-   ##p = [name, price]
+   #p = [name, price]
    products.append([name, price]) # add p into product list
 print(products)
 
 print(products [0][0]) ## find the 1st column in 1st row
+##------------------------------------------------------------##
+for q in products:
+    print(q[0],'price is:', q[1])
 
-for p in products:
-    print(p[0],'price is:', p[1])
+#write product into a string file txt    
+
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+    #f.write('product' + ',' + 'price' + '\n')
+    f.write('product, price\n')
+    for r in products:
+        f.write(r[0] + ',' + r[1] + '\n')
     
